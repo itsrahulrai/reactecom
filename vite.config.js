2cd16bead898f9a18c0837e07c9c2ffbd2c9d352
Rahul Rai <therahulray@gmail.com>
2025-04-03 18:12:46 +0530

    UPDATED SLOVED ERROR TO DISPLAY PROPER WAY

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/reactecom/', // GitHub Pages base path (replace 'ecommerce' with your repo name)
+  plugins: [react(), tailwindcss()],
+  base: '/reactecom/',  // Set this to your GitHub repository name
 });
