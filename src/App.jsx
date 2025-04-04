c5bb23952c87c6af063e1e050711f151d8147374
Rahul <50365352+itsrahulrai@users.noreply.github.com>
2025-04-04 12:43:26 +0530

    Update App.jsx
    add basename in App

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
+    <Router  basename="/reactecom">
       <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />} />
